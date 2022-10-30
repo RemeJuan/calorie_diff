c3459fec050a0bfb0388bef0b783cb2c0657d3f6
Reme Le Hane <mail@remej.dev>
2022-10-30 08:48:10 +0200

    feat(app): updated main layout

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,9 @@
+import 'package:calorie_diff/widgets/current_calories/current_calories.dart';
+import 'package:calorie_diff/widgets/historic_calories/historic_calories.dart';
 import 'package:calorie_diff/widgets/permissions_request.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'health/health_providers.dart';
 
@@ -11,28 +14,45 @@ class CalorieDiffApp extends ConsumerWidget {
   Widget build(context, ref) {
     return MaterialApp(
       title: 'Calorie Diff',
-      theme: ThemeData.dark(),
+      theme: _buildTheme(Brightness.dark),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Calorie Diff'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () {
+                ref.invalidate(healthDataProvider);
+                ref.invalidate(historicHealthDataProvider);
+              },
+            ),
+          ],
         ),
         body: Container(
-          alignment: Alignment.center,
+          alignment: Alignment.topCenter,
           padding: const EdgeInsets.all(16),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const PermissionRequest(),
-              ElevatedButton(
-                onPressed: () async {
-                  final data = ref.watch(healthDataProvider);
-                  print(data);
-                },
-                child: const Text('Get Health Data'),
-              ),
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              PermissionRequest(),
+              CurrentCalories(),
+              HistoricCalories(),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  ThemeData _buildTheme(brightness) {
+    var baseTheme = ThemeData(brightness: brightness);
+
+    return baseTheme.copyWith(
+      textTheme: GoogleFonts.poppinsTextTheme(baseTheme.textTheme),
+      scaffoldBackgroundColor: const Color.fromRGBO(23, 31, 44, 1),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color.fromRGBO(23, 31, 44, 1),
+        elevation: 0,
       ),
     );
   }
