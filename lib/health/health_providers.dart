01d7b4b343cfd1b2b65a02b92e3074dd9ec9d094
Reme Le Hane <mail@remej.dev>
2022-10-30 08:46:07 +0200

    feat(providers): provider for historic data

--- lib/health/health_providers.dart
+++ lib/health/health_providers.dart
@@ -1,13 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:health/health.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'health_data_model.dart';
+import '../models/health_data_model.dart';
 import 'health_utils.dart';
-
-final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
-  throw UnimplementedError();
-});
 
 final healthFactoryProvider = Provider<HealthFactory>((ref) => HealthFactory());
 final healthRequestAccessProvider = FutureProvider<bool?>((ref) {
@@ -17,9 +12,12 @@ final healthRequestAccessProvider = FutureProvider<bool?>((ref) {
   return health.requestAuthorization(types);
 });
 final hasHealthDataAccessProvider = FutureProvider<bool?>((ref) async {
-  final types = ref.watch(healthDataTypesProvider);
+  final types = ref.read(healthDataTypesProvider);
 
-  return HealthFactory.hasPermissions(types);
+  return await HealthFactory.hasPermissions(
+    types,
+    permissions: [HealthDataAccess.READ],
+  );
 });
 
 final healthDataTypesProvider = Provider<List<HealthDataType>>((ref) {
@@ -31,28 +29,71 @@ final healthDataTypesProvider = Provider<List<HealthDataType>>((ref) {
 });
 
 final healthDataProvider = FutureProvider<HealthDataModel>((ref) async {
+  final now = DateTime.now();
   final health = ref.read(healthFactoryProvider);
   final types = ref.read(healthDataTypesProvider);
 
   final data = await health.getHealthDataFromTypes(
-    DateTime.now().subtract(const Duration(days: 1)),
-    DateTime.now(),
+    DateTime(now.year, now.month, now.day),
+    now,
     types,
   );
+
   final clean = HealthFactory.removeDuplicates(data);
 
   return HealthDataModel(
-    active: HealthUtils.combineData(HealthUtils.groupData(
+    date: now,
+    active: HealthUtils.prepareDataEntry(
       clean,
       HealthDataType.ACTIVE_ENERGY_BURNED,
-    )),
-    rest: HealthUtils.combineData(HealthUtils.groupData(
+    ),
+    rest: HealthUtils.prepareDataEntry(
       clean,
       HealthDataType.BASAL_ENERGY_BURNED,
-    )),
-    dietary: HealthUtils.combineData(HealthUtils.groupData(
+    ),
+    dietary: HealthUtils.prepareDataEntry(
       clean,
       HealthDataType.DIETARY_ENERGY_CONSUMED,
-    )),
+    ),
   );
+});
+
+final historicHealthDataProvider =
+    FutureProvider<List<HealthDataModel>>((ref) async {
+  final now = DateTime.now();
+  final start = now.subtract(const Duration(days: 7));
+  final end = DateTime(now.year, now.month, now.day);
+
+  final health = ref.read(healthFactoryProvider);
+  final types = ref.read(healthDataTypesProvider);
+
+  final data = await health.getHealthDataFromTypes(
+    DateTime(start.year, start.month, start.day),
+    end,
+    types,
+  );
+
+  final clean = HealthFactory.removeDuplicates(data);
+  final dates = HealthUtils.getDates(clean);
+
+  return dates.map((date) {
+    final active = HealthUtils.prepareDataEntry(
+      HealthUtils.filterByDate(clean, date),
+      HealthDataType.ACTIVE_ENERGY_BURNED,
+    );
+    final rest = HealthUtils.prepareDataEntry(
+      HealthUtils.filterByDate(clean, date),
+      HealthDataType.BASAL_ENERGY_BURNED,
+    );
+    final dietary = HealthUtils.prepareDataEntry(
+      HealthUtils.filterByDate(clean, date),
+      HealthDataType.DIETARY_ENERGY_CONSUMED,
+    );
+    return HealthDataModel(
+      date: date,
+      active: active,
+      rest: rest,
+      dietary: dietary,
+    );
+  }).toList();
 });
