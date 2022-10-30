11d0f88d100e88ff932566c867d9217ff0e159cf
Reme Le Hane <mail@remej.dev>
2022-10-30 08:47:50 +0200

    chore(app): removed references to shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
-import 'health/health_providers.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final prefs = await SharedPreferences.getInstance();
-
   runApp(
-    ProviderScope(
-      overrides: [
-        sharedPreferencesProvider.overrideWithValue(prefs),
-      ],
-      child: const CalorieDiffApp(),
+    const ProviderScope(
+      overrides: [],
+      child: CalorieDiffApp(),
     ),
   );
 }
