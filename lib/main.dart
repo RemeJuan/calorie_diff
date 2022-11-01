059d69203e5f0fc4a4257c9b987a021944874801
Reme Le Hane <mail@remej.dev>
2022-11-01 08:05:19 +0200

    feat(storage): setting up shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:calorie_diff/route_delegate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'app.dart';
+import 'core/core_providers.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final themeData = ThemeData(brightness: Brightness.dark);
+  final prefs = await SharedPreferences.getInstance();
 
   runApp(
     ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(prefs),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Calorie Diff',
@@ -22,7 +28,7 @@ void main() async {
             elevation: 0,
           ),
         ),
-        home: const CalorieDiffApp(),
+        home: const RouteDelegate(),
       ),
     ),
   );
