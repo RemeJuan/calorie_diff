5b73b2cec5e9d53868320cd2bf63a71a429b22a1
Reme Le Hane <mail@remej.dev>
2022-11-12 18:19:48 +0200

    feat(app): added range picker

---
+++ lib/widgets/range_picker/range_picker.dart
@@ -0,0 +1,35 @@
+import 'package:calorie_diff/health/health_providers.dart';
+import 'package:calorie_diff/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class RangePicker extends HookConsumerWidget {
+  final int selectedValue;
+
+  const RangePicker({required this.selectedValue, Key? key}) : super(key: key);
+
+  @override
+  Widget build(context, ref) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: DropdownButton<int>(
+        isExpanded: true,
+        isDense: true,
+        dropdownColor: AppTheme.barColor,
+        underline: const SizedBox.shrink(),
+        items: [7, 14, 30, 60, 90].map(_generateItem).toList(),
+        onChanged: (value) {
+          ref.read(selectedDaysProvider.notifier).state = value!;
+        },
+        value: selectedValue,
+      ),
+    );
+  }
+
+  DropdownMenuItem<int> _generateItem(int value) {
+    return DropdownMenuItem(
+      value: value,
+      child: Text('$value days'),
+    );
+  }
+}
---
+++ test/widgets/range_picker/range_picker_test.dart
@@ -0,0 +1,40 @@
+import 'package:calorie_diff/widgets/range_picker/range_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../test_helpers.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('should render default picker', (tester) async {
+    //arrange
+    await tester.pumpApp(
+      const RangePicker(selectedValue: 7),
+      [],
+    );
+    //act
+    await tester.pumpAndSettle();
+    //assert
+    expect(find.text("7 days"), findsOneWidget);
+    expect(find.byType(DropdownMenuItem<int>), findsNWidgets(5));
+  });
+
+  testWidgets('should update selected date range', (tester) async {
+    //arrange
+    await tester.pumpApp(
+      const RangePicker(selectedValue: 7),
+      [],
+    );
+    //act
+    final buttonFinder = find.byType(DropdownButton<int>);
+    await tester.tap(buttonFinder);
+    await tester.pump();
+
+    await tester.tap(find.text("14 days").last);
+    await tester.pumpAndSettle();
+    //assert
+    expect(find.text("14 days"), findsOneWidget);
+    expect(find.byType(DropdownMenuItem<int>), findsNWidgets(5));
+  });
+}
