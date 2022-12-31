24d8df8ea630b5cf28c388803966328936771681
Reme Le Hane <mail@remej.dev>
2022-12-31 18:34:50 +0200

    feat(settings): added extension on text controller for selecting all text

--- lib/core/extensions.dart
+++ lib/core/extensions.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/widgets.dart';
+
 extension ExtendedDateTime on DateTime {
   static DateTime? _customTime;
 
@@ -7,5 +9,12 @@ extension ExtendedDateTime on DateTime {
 
   static set customTime(DateTime customTime) {
     _customTime = customTime;
+  }
+}
+
+extension TextEditingControllerExt on TextEditingController {
+  void selectAll() {
+    if (text.isEmpty) return;
+    selection = TextSelection(baseOffset: 0, extentOffset: text.length);
   }
 }
