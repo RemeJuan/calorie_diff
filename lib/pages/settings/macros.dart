e97319b96a133bde847e042f4385322214fb7d68
Reme Le Hane <mail@remej.dev>
2023-01-01 15:15:46 +0200

    fix(app): decouple settings button from macros visibility.

--- lib/pages/settings/macros.dart
+++ lib/pages/settings/macros.dart
@@ -1,40 +1,44 @@
 import 'package:calorie_diff/core/extensions.dart';
 import 'package:calorie_diff/providers/settings_providers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SettingsMacros extends ConsumerWidget {
+class SettingsMacros extends HookConsumerWidget {
   const SettingsMacros({Key? key}) : super(key: key);
 
   @override
   Widget build(context, ref) {
     final settings = ref.watch(settingsProvider);
     final macros = settings.macros;
+
     final carbController = TextEditingController(text: macros.carb.toString());
     final fatController = TextEditingController(text: macros.fat.toString());
     final proteinController = TextEditingController(
       text: macros.protein.toString(),
     );
 
+    final isEnabled = useState(settings.macrosEnabled);
+
     return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             const Text("Enable Macros"),
             Switch(
-              value: settings.macrosEnabled,
+              value: isEnabled.value,
               activeColor: Colors.lightBlue,
               onChanged: (value) {
-                final updates = settings.copyWith(macrosEnabled: value);
-                return ref.refresh(updateSettingsProvider(updates));
+                isEnabled.value = value;
               },
             ),
           ],
         ),
         const SizedBox(height: 16),
         Visibility(
-          visible: settings.macrosEnabled,
+          visible: isEnabled.value,
           child: Column(
             key: const ValueKey("macros"),
             children: [
@@ -82,36 +86,38 @@ class SettingsMacros extends ConsumerWidget {
                   ),
                 ],
               ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.lightBlue,
-                  minimumSize: const Size.fromHeight(40),
-                ),
-                onPressed: () {
-                  final carb = carbController.text;
-                  final fat = fatController.text;
-                  final protein = proteinController.text;
-                  final updates = settings.copyWith(
-                    macros: settings.macros.copyWith(
-                      carb: double.parse(carb),
-                      fat: double.parse(fat),
-                      protein: double.parse(protein),
-                    ),
-                  );
-                  FocusScope.of(context).unfocus();
-                  return ref.refresh(updateSettingsProvider(updates));
-                },
-                child: const Text(
-                  "Save",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
             ],
+          ),
+        ),
+        const Expanded(child: SizedBox.shrink()),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.lightBlue,
+            minimumSize: const Size.fromHeight(40),
+          ),
+          onPressed: () {
+            final carb = carbController.text;
+            final fat = fatController.text;
+            final protein = proteinController.text;
+            final updates = settings.copyWith(
+              macrosEnabled: isEnabled.value,
+              macros: settings.macros.copyWith(
+                carb: double.parse(carb),
+                fat: double.parse(fat),
+                protein: double.parse(protein),
+              ),
+            );
+            FocusScope.of(context).unfocus();
+            return ref.refresh(updateSettingsProvider(updates));
+          },
+          child: const Text(
+            "Save",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ],
