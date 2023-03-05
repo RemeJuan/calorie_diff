8412dd88b5b781ac123426d60ffd9ad1c398ffee
Reme Le Hane <mail@remej.dev>
2023-03-05 09:43:20 +0200

    test(header): adding setting mock for test

--- test/widgets/historic_calories/header_row_test.dart
+++ test/widgets/historic_calories/header_row_test.dart
@@ -1,3 +1,5 @@
+import 'package:calorie_diff/models/settings_model.dart';
+import 'package:calorie_diff/providers/settings_providers.dart';
 import 'package:calorie_diff/widgets/historic_calories/header_row.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -8,9 +10,14 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('should render HeaderRow', (tester) async {
-    await tester.pumpApp(const HeaderRow(
-      entries: ['Date', 'In', 'Out', 'Diff'],
-    ));
+    await tester.pumpApp(
+      const HeaderRow(
+        entries: ['Date', 'In', 'Out', 'Diff'],
+      ),
+      [
+        settingsProvider.overrideWith((_) => SettingsModel.initial()),
+      ],
+    );
 
     expect(find.byType(Text), findsNWidgets(4));
   });
